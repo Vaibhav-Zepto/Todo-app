79cf4d0d08a93e852c7b3a3cc0b948df573378ce
psionic-08 <int-vaibhav.chawlaa@zeptonow.com>
2024-05-17 18:03:04 +0530

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react'; 
 function App() {
+  const [task,setTask]= useState();
+  const [list,setList]= useState([]);
+  let nextId=0;
+  const addTask = (task)=>{
+    if(task!==''){
+      nextId++;
+      setList([...list, {id:nextId,data:task}]);
+    }
+  }
+  const removeTask = (id)=>{
+    setList(
+      list.filter(task=> task.id!==id)
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="add">
+        <input className="entry" value={task} onChange={(e)=>setTask(e.target.value)}></input>
+        <button className="addButton"onClick={()=>addTask(task)}>add tasks</button>
+      </div>
+      <div className="tasks">
+        {
+          list.map((item)=>{
+            return(
+              <div className='taskEntry' key={item.id}>
+                <div className='taskData'>{item.data}</div>
+                <button onClick={()=>removeTask(item.id)}>remove task</button>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
